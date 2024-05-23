282c4c87e448ffdf1519fc46b45ebc4a610496e9
yosuke <yosuke20sasami@gmail.com>
2024-05-23 12:51:03 +0900

    面接一覧画面作成

--- app.py
+++ app.py
@@ -1,20 +1,43 @@
 import streamlit as st # Streamlitというpython用のUIを編集できるフレームワーク
 import datetime
-import random
 import requests
 import json
+import pandas as pd
 
-st.title('APIテスト画面')
+st.title('面接予約画面')
 
+st.write('#### 面接一覧')
+# 面接一覧取得
+url_interviews = 'http://127.0.0.1:8000/interviews'
+res = requests.get(url_interviews)
+interviews = res.json()
+df_interviews = pd.DataFrame(interviews)
+
+# 日時のフォーマットを見やすい形に変換
+to_datetime = lambda x: datetime.datetime.fromisoformat(x).strftime('%Y/%m/%d %H:%M')
+
+# 面接開始日時の列に適用
+df_interviews['interview_datetime'] = df_interviews['interview_datetime'].map(to_datetime)
+
+df_interviews = df_interviews.rename(columns={
+  'company_name': '企業名',
+  'interview_datetime': '面接開始日時',
+  'location': '面接場所',
+  'interview_id': '面接id'
+}) 
+st.table(df_interviews)
+
+st.write('#### 面接登録')
+# 面接一覧取得
 with st.form(key='interview'):
-  interview_id: int = random.randint(0, 10)
+  # interview_id: int = random.randint(0, 10)
   company_name: str = st.text_input('企業名', max_chars=50)
   date = st.date_input('日付: ', min_value=datetime.date.today())
   # streamlitに日時型がないので、日付型と時間型を分けて作って後でマージする
   start_time = st.time_input('開始時刻: ', value=datetime.time(hour=10, minute=0))
   location: str = st.text_input('面接場所', value='オンライン')
   data = {
-    'interview_id': interview_id,
+    # 'interview_id': interview_id,
     'company_name': company_name,
     'interview_datetime': datetime.datetime(
       year=date.year,
@@ -25,16 +48,15 @@ with st.form(key='interview'):
     ).isoformat(),
     'location': location
   }
-  submit_button = st.form_submit_button(label='リクエスト送信')
+  submit_button = st.form_submit_button(label='面接登録')
 
 if submit_button:
-  st.write('## 送信データ')
-  st.json(data)
-  st.write('## レスポンス結果')
   url = 'http://127.0.0.1:8000/interviews'
   res = requests.post(
     url,
     data=json.dumps(data)
   )
+  if res.status_code == 200:
+    st.success('面接登録完了')
   st.write(res.status_code)
   st.json(res.json())
---
+++ sql_app/main.py
@@ -0,0 +1,34 @@
+from typing import List
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+
+from . import crud, models, schemas
+from .database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+def get_db():
+  db = SessionLocal()
+  try:
+    yield db
+  finally:
+    db.close()
+
+# API部分
+
+# @app.get("/")
+# async def index():
+#   return {"message": "Success!!!!"}
+
+# Read
+@app.get("/interviews", response_model=List[schemas.Interview])
+async def read_interviews(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+  interviews = crud.get_interviews(db, skip=skip, limit=limit)
+  return interviews
+
+# Create
+@app.post("/interviews", response_model=schemas.Interview)
+async def create_interview(interview: schemas.InterviewCreate, db: Session = Depends(get_db)):
+  return crud.create_interview(db=db, interview=interview)
